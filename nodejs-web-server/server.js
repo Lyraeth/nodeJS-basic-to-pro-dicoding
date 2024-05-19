947094b0446ff9c125898134e98c4c8659177629
Mahsa Nurfarhan Hidayat <nurfarhan52@gmail.com>
2024-05-19 20:02:41 +0700

    refactor: foldering for new module

---
+++ nodejs-web-server/server.js
@@ -0,0 +1,73 @@
+const http = require("http");
+const { chunk } = require("lodash");
+
+const requestListener = (request, response) => {
+  // response.setHeader("Content-Type", "text/html");
+  response.setHeader("Content-Type", "application/json");
+  response.setHeader("Powered-By", "Node.js");
+
+  const { method, url } = request;
+
+  if (url === "/") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "Ini adalah Homepage!",
+        })
+      );
+    } else {
+      response.statusCode = 400;
+      response.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({ message: "Halo! Ini adalah halaman about" })
+      );
+    } else if (method === "POST") {
+      let body = [];
+
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.statusCode = 200;
+        response.end(
+          JSON.stringify({ message: `Halo, ${name}! Ini adalah halaman about` })
+        );
+      });
+    } else {
+      response.statusCode = 400;
+      response.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
+      );
+    }
+  } else {
+    response.statusCode = 404;
+    response.end(
+      JSON.stringify({
+        message: "Halaman tidak ditemukan",
+      })
+    );
+  }
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan dengan baik pada PORT : ${port}`);
+});
